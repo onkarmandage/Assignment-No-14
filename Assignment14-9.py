8fdc14bb43e174db89b3fbef5b8d3a95891b3065
onkarmandage <onkarmandage@gmail.com>
2026-07-17 13:54:54 +0530

    Add files via upload

---
+++ Assignment14-9.py
@@ -0,0 +1,10 @@
+Multiplication=lambda No1,No2: No1*No2
+
+def main():
+    Value1=int(input("Enter the first no "))
+    Value2=int(input("Enter the second no "))
+    Ret=Multiplication(Value1,Value2)
+    print("Multiplication is ",Ret)
+
+if __name__=="__main__":
+    main()
